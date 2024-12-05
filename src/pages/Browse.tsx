d9dd00d8d6e2272e3c3258612a81986e31f8214f
Dhimas Fajarw <ghidet4pcoop@gmail.com>
2024-12-06 06:26:03 +0700

    feat(wrapper): extract office section from Browse file to BrowseOfficeWrapper

--- src/pages/Browse.tsx
+++ src/pages/Browse.tsx
@@ -1,6 +1,5 @@
-
-import OfficeCard from "../components/OfficeCard";
 import BrowseCityWrapper from "../wrappers/BrowseCityWrapper";
+import BrowseOfficeWrapper from "../wrappers/BrowseOfficeWrapper";
 
 export default function Browse() {
   return (
@@ -270,27 +269,7 @@ export default function Browse() {
           </div>
         </div>
       </section>
-      <section
-        id="Fresh-Space"
-        className="flex flex-col gap-[30px] w-full max-w-[1130px] mx-auto mt-[100px] mb-[120px]"
-      >
-        <h2 className="font-bold text-[32px] leading-[48px] text-nowrap text-center">
-          Browse Our Fresh Space.
-          <br />
-          For Your Better Productivity.
-        </h2>
-        <div className="grid grid-cols-3 gap-[30px]">
-          <OfficeCard></OfficeCard>
-          <OfficeCard></OfficeCard>
-          <OfficeCard></OfficeCard>
-          <OfficeCard></OfficeCard>
-          <OfficeCard></OfficeCard>
-          <OfficeCard></OfficeCard>
-          <OfficeCard></OfficeCard>
-          <OfficeCard></OfficeCard>
-          <OfficeCard></OfficeCard>
-        </div>
-      </section>
+      <BrowseOfficeWrapper/>
     </>
   );
 }
